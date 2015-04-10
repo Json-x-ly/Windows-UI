0b0ad67239eaf51e052001b1aff8f34e600c1319
Rojibora <Rojibora@gmail.com>
2015-04-09 19:42:23 -0700

    QR Code Is printing at press of a button taking user input

--- Windows UI APP/Windows UI APP/MainWindow.xaml.cs
+++ Windows UI APP/Windows UI APP/MainWindow.xaml.cs
@@ -12,12 +12,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Interop;
+using QRCoder;
 
 namespace Windows_UI_APP
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -25,7 +28,29 @@ namespace Windows_UI_APP
             InitializeComponent();
         }
 
+        private BitmapSource Bitmap2BitmapImage(System.Drawing.Bitmap bitmap)
+        {
+            BitmapSource i = Imaging.CreateBitmapSourceFromHBitmap(
+                           bitmap.GetHbitmap(),
+                           IntPtr.Zero,
+                           Int32Rect.Empty,
+                           BitmapSizeOptions.FromEmptyOptions());
+            return i;
+        }
+
         private void Generate_QR_Codes(object sender, RoutedEventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeGenerator.QRCode qrCode = qrGenerator.CreateQrCode(_TextBox_01.Text, QRCodeGenerator.ECCLevel.Q);
+            pictureBoxQRCode.Source = Bitmap2BitmapImage(qrCode.GetGraphic(20)); ;
+        }
+
+        private void hmm(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void _TextBox_01_Copy_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
